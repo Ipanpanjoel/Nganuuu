36c4d5cb91cc0650469ff5925c1a89fb96a90356
Ipanpanjoel <34146259+Ipanpanjoel@users.noreply.github.com>
2017-12-01 14:43:21 +0700

    Update fft.js

--- fft.js
+++ fft.js
@@ -12,11 +12,11 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'annastasya_hakim',
+    password: 'X-Ray123'
 }
 
-const TargetUsername = '';
+const TargetUsername = 'princessyahrini';
 
 const Login = async function(User){
 
