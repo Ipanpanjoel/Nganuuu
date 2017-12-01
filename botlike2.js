a9d9935fd076123d54cca0b07bad5da2574e5a42
Ipanpanjoel <34146259+Ipanpanjoel@users.noreply.github.com>
2017-12-01 14:45:26 +0700

    Update botlike2.js

--- botlike2.js
+++ botlike2.js
@@ -10,8 +10,8 @@ const delay = require('delay');
 const chalk = require('chalk');
 
 var User = {
-    username: '',
-    password: ''
+    username: 'annastasya_hakim',
+    password: 'X-Ray123'
 }
 
 const onlyUnique = async function(value, index, self) { 
